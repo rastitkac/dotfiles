5b9cdbac45c8e1b3d0a58a3d87cfc9077c467df5
Rasti Tkac <info@rastitkac.net>
2021-04-17 15:20:05 +0200

    Add wpsrlocal

--- fish/functions/wpsrlocal.fish
+++ fish/functions/wpsrlocal.fish
@@ -1,6 +1,6 @@
 function wpsrlocal --description 'WP Search Replace local'
     command wp option get siteurl
-    command wp search-replace (string replace https: '' (wp option get siteurl)) (echo '//'(basename $PWD)'.test')
-    command wp search-replace (echo 'http://'(basename $PWD)'.test') (echo 'https://'(basename $PWD)'.test')
+    command wp search-replace (string replace https: '' (wp option get siteurl)) (echo '//'(basename $PWD)'.test') $argv
+    command wp search-replace (echo 'http://'(basename $PWD)'.test') (echo 'https://'(basename $PWD)'.test') $argv
     command wp option get siteurl
 end
