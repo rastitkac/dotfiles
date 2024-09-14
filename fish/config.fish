40cd64dc14e9bb7705d8215e95123a1e05f8fc2a
Rasti Tkac <rasti@webkinder.ch>
2024-09-14 11:43:28 +0200

    Edit config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,9 @@ if test (uname -s) = "Darwin"
   set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
 end
 
-set -x -g PATH (/opt/homebrew/bin/brew shellenv) $PATH
+fish_add_path /opt/homebrew/bin
+
+# set -x -g PATH (/opt/homebrew/bin/brew shellenv) $PATH
 
 set -U fish_key_bindings fish_vi_key_bindings
 
@@ -11,14 +13,6 @@ set -x -g TERM "xterm-256color"
 
 set -x -g LC_ALL en_GB.UTF-8
 set -x -g LANG en_GB.UTF-8
-
-# Coreutils bin and man folders
-set -x -g PATH (brew --prefix coreutils)/libexec/gnubin $PATH
-# set -x -g MANPATH (brew --prefix coreutils)/libexec/gnuman $MANPATH
-
-# Findutils bin and man folders
-set -x -g PATH (brew --prefix findutils)/libexec/gnubin $PATH
-# set -x -g MANPATH (brew --prefix findutils)/libexec/gnuman $MANPATH
 
 # go bin folder
 set -x -g PATH ~/go/bin $PATH
